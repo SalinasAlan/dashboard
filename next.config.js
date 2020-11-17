a504b9fa16181aea706345f9f86dfe17ed3c2c8d
Alan <alansal_11@hotmail.com>
2020-11-17 02:41:04 -0600

    Environment variables were added

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        FIRABESE_API_KEY: process.env.API_KEY,
+        FIRABESE_AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+        FIRABESE_DATABASE_URL: process.env.DATABASE_URL,
+        FIRABESE_PROYECT_ID: process.env.PROYECT_ID,
+        FIRABESE_STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+        FIRABESE_MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+        FIRABESE_APP_ID: process.env.APP_ID,
+        FIRABESE_MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+        MAP_TOKEN: process.env.MAP_TOKEN
+    }
+}
