012e9233281c8bb457f29b9c8e2e240b44faff8c
Alan <alansal_11@hotmail.com>
2020-11-17 02:43:06 -0600

    ThemeProvider for styled-components was added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/styled';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
