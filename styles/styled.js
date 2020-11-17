474b04b12b32022f62796cf3a04d06579a70265d
Alan <alansal_11@hotmail.com>
2020-11-17 02:39:13 -0600

    Added the colors for the app

---
+++ styles/styled.js
@@ -0,0 +1,9 @@
+
+export const theme = {
+    primary: '#2f3a43',
+    // primary: '#323432',
+    textColor: '#DBFFEA',
+    textHoverColor: '#FFE0AD',
+    textHoverColorSecondary: '#FFBD50',
+    locationIconColor: '#FF5757'
+}
